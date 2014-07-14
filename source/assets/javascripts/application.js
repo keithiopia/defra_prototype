8cad850a7e7dcc79167b6129828317d907dbbc18
Tim P <contact@timpaul.co.uk>
2014-07-14 12:17:15 +0100

    Add land-use-list auto-suggest field

---
+++ source/assets/javascripts/application.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+
+
+  // Clicking anywhere in the table row activate the link
+  $('.parcels tbody tr').click(function(){
+    var ref = $(this).find('a').attr('href');
+  });
+
+  // Convert land-use list into an auto-suggest box
+  $("#land-use-list").chosen();
+
+
+});
